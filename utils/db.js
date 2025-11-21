32c2fb3514025b38dc15840da27a544bc73cd44b
Thanigaivel Muthusamy <41562974+thanigaiguru@users.noreply.github.com>
2025-11-21 07:48:09 +0000

    db check

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,13 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGO_URL;
-const options = {};
 
 let client;
 let clientPromise;
 
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, options);
-  global._mongoClientPromise = client.connect(); // cache the promise globally
+  client = new MongoClient(uri, {});
+  global._mongoClientPromise = client.connect();
 }
 
 clientPromise = global._mongoClientPromise;
